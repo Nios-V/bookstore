95d38414cbe82bc87cb1e099a429eac53ad0795c
Nios-V <niosv.1104@gmail.com>
2025-04-09 06:47:34 -0400

    add getters / setters to entities

--- src/main/java/com/book/store/bookstore/entities/Book.java
+++ src/main/java/com/book/store/bookstore/entities/Book.java
@@ -20,4 +20,61 @@ public class Book {
     private LocalDate publicationDate;
     private Author author;
     private Category category;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    
 }
--- src/main/java/com/book/store/bookstore/entities/Category.java
+++ src/main/java/com/book/store/bookstore/entities/Category.java
@@ -12,4 +12,25 @@ public class Category {
     private Long id;
     private String nombre;
     private String description;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
 }
